3e120fe477017f8b0746c54c6990f85575050730
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-05-26 20:57:02 -0300

    estrutura inicial

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const nameAthlete = document.querySelector('#name')
+const position = document.querySelector('#position')
+
+const insertCard = addEventListener('keyup', () => {
+  const athlete = nameAthlete.value
+  console.log(athlete)
+})
